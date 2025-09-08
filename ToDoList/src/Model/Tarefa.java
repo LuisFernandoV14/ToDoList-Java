ec3f141fe4ad99f861cc5365e612041a4b551552
Lucas C. S. <lucasscosilva@gmail.com>
2025-09-08 18:57:44 -0300

    Definindo as funções da Controller

--- ToDoList/src/Model/Tarefa.java
+++ ToDoList/src/Model/Tarefa.java
@@ -3,7 +3,7 @@ package Model;
 import java.time.LocalDateTime;
 
 public class Tarefa {
-	private Long id;
+	private int id;
 	private String titulo;
 	private String descricao;
 	private LocalDateTime data_inicio;
@@ -11,11 +11,24 @@ public class Tarefa {
 	private Boolean concluido;
 	
 	public Tarefa() {
+		this.id = 0;
+		this.titulo = "Insira o título";
+		this.descricao = "Insira a descricao aqui"; 
+		this.data_inicio = LocalDateTime.now();
+		this.concluido = false;
+	}
+
+	public Tarefa(int id,String titulo, String descricao) {
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao; 
 		this.data_inicio = LocalDateTime.now();
 		this.concluido = false;
 	}
 	
-	public Tarefa(Long id, String titulo) {
+	public Tarefa(int id, String titulo) {
 		this.id = id;
+		this.titulo = titulo;
+		this(id, titulo, "");
 	}
 }
--- ToDoList/src/Services/TarefasControl.java
+++ ToDoList/src/Services/TarefasControl.java
@@ -1,5 +1,20 @@
 package Services;
+import Model.*;
 
 public class TarefasControl {
+    public void criarTarefa (){
 
+    };
+
+    public void deletarTarefa (){
+
+    };
+
+    public void editarTitulo (){
+
+    };
+
+    public void editarDescricao (){
+        
+    };
 }
