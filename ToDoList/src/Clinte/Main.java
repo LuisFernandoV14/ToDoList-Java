9e29a07ab9e9fd376b6c874685e94e31b3436565
Lucas C. S. <lucasscosilva@gmail.com>
2025-09-08 20:50:00 -0300

    Instanciando objeto de controle na view

--- ToDoList/src/Clinte/Main.java
+++ ToDoList/src/Clinte/Main.java
@@ -5,8 +5,6 @@ import Services.TarefasControl;
 import java.util.ArrayList;
 
 public class Main {
-
-
     ArrayList<Tarefa> model = new ArrayList<>();
     TarefasControl controller = new TarefasControl(model);
 
